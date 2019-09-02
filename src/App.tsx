67060a228250063f035c3e689aeb6f6bf183b445
Robert Janaszek <janaszer@gmail.com>
2019-09-02 20:20:58 +0200

    🔨 Dragging with mouse down and up

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 interface IState {
+  isDragging: boolean;
   x: number;
   y: number;
 }
@@ -13,14 +14,18 @@ export class App extends React.Component<{}, IState> {
   constructor() {
     super({});
     this.state = {
+      isDragging: false,
       x: 0,
       y: 0,
     };
   }
 
   private onMouseDrag(event: React.MouseEvent) {
-    const elem: SVGGraphicsElement = event.target as SVGGraphicsElement;
+    if (!this.state.isDragging) {
+      return;
+    }
 
+    const elem: SVGGraphicsElement = event.target as SVGGraphicsElement;
     const CTM = elem.getScreenCTM();
     if (!CTM) {
       return;
@@ -28,15 +33,29 @@ export class App extends React.Component<{}, IState> {
 
     this.setState({
       x: (event.clientX - CTM.e) / CTM.a,
-      y: (event.clientY - CTM.f) / CTM.d
+      y: (event.clientY - CTM.f) / CTM.d,
+    });
+  }
+
+  private dragStart() {
+    this.setState({
+      isDragging: true,
+    });
+  }
+
+  private dragEnd() {
+    this.setState({
+      isDragging: false,      
     });
   }
 
   public render() {
     return (
       <div className="App">
-        <svg width="1000" height="500" viewBox="0 0 2000 1000"
+        <svg width="1000" height="500" viewBox="0 0 1000 500"
           onMouseMove={(event: React.MouseEvent) => this.onMouseDrag(event)}
+          onMouseDown={() => this.dragStart()}
+          onMouseUp={() => this.dragEnd()}
         >
           <rect
             x={this.state.x}
